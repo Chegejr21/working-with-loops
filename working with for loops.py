1ce3007ed779b8b503fe178c6b6a562019eef77a
I am X <77246144+Chegejr21@users.noreply.github.com>
2021-12-26 15:07:17 +0300

    Update working with for loops.py

--- working with for loops.py
+++ working with for loops.py
@@ -1,4 +1,5 @@
 
+#working on dict using for loops
 Number_of_animals = {"dogs": 30, "cows": 40, "cats": 12, "sheep": 55}
 print(type(Number_of_animals))
 print(Number_of_animals.values())
@@ -16,20 +17,23 @@ x = [z + " for geeks"]
 print(x)
 for i in x:
     print(i)
-
+#finding the sum of numbers in a given list
 sum = 0
 numbers = [1, 2, 3, 56, 45, 89, 90, 12, 23, 24]
 for y in numbers:
     sum = sum + y
     print("The total sum is ", sum)
-
+    
+#prints odd numbers between 1 and 100
 for c in range(1, 100, 2):
     print(c)
+    
+ #prints out even numbers between 2 and 100   
 for t in range(2, 100, 2):
     print(t)
     
 
-
+#checking out the divisibility test of 2 between 50 and 100
 for i in range(50, 100):
     if i % 2 == 0:
         print(i, " is divisible by 2")
